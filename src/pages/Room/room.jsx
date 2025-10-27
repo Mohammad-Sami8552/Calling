fd5eb9964476c357434bcf5410fc4756b3cacd96
Mohammad-Sami8552 <mohammadsami8552@gmail.com>
2025-10-27 22:31:05 +0530

    updated url for copy link

--- src/pages/Room/room.jsx
+++ src/pages/Room/room.jsx
@@ -15,7 +15,7 @@ const RoomPage = () => {
           sharedLinks : [
             {
                 name : "Copy Link",
-                url : `http://localhost:3000/room/${roomId}`
+                url : `https://calling-theta.vercel.app/room/${roomId}`
             }
           ],
           scenario: {
